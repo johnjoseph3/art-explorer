c2e00a38b209603cf5e066fff8e0e22872a9b18e
Joseph, John <jjoseph@cardinalsolutions.com>
2017-07-13 13:51:10 -0400

    Change api call size.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,6 +3,7 @@ import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
+const apiCallSize = 40;
 const clientId = '56bde6989fb5615bb776';
 const clientSecret = '322f806dfecd4a7615548e87240f5bdd';
 
@@ -29,7 +30,7 @@ export class DataService {
     let options = new RequestOptions({ headers: headers });
     const offset = Math.floor(Math.random() * (300 - 0 + 1) + 0);
 
-    return this.http.get(`https://api.artsy.net:443/api/artworks?offset=${offset}&size=20`, options)
+    return this.http.get(`https://api.artsy.net:443/api/artworks?offset=${offset}&size=${apiCallSize}`, options)
       .map( (response: Response) => response.json())
       .toPromise()
       .then(data => {
@@ -42,7 +43,7 @@ export class DataService {
     headers.append('X-XAPP-Token', localStorage.getItem('apiToken'));
     let options = new RequestOptions({ headers: headers });
 
-    return this.http.get(`${href}&size=20`, options)
+    return this.http.get(`${href}&${apiCallSize}`, options)
       .map( (response: Response) => response.json())
       .toPromise()
       .then(data => {
